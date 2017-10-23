daad031626a0bfae42fe3cbdfb259e30d3433941
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-10-23 20:05:05 +0100

    Make Gender not an enum class

--- app/src/main/kotlin/co/familytreeapp/model/Gender.kt
+++ app/src/main/kotlin/co/familytreeapp/model/Gender.kt
@@ -1,6 +1,21 @@
 package co.familytreeapp.model
 
-enum class Gender(val id: Int) {
-    MALE(0),
-    FEMALE(1)
+/**
+ * Represents a gender.
+ *
+ * @param id    an integer identifier corresponding to a gender. 0 = male; 1 = female.
+ */
+data class Gender(val id: Int) {
+
+    init {
+        if (id !in 0..1) {
+            throw IllegalArgumentException("the id for Gender must be between 0 and 1")
+        }
+    }
+
+    companion object {
+        @JvmField val MALE = Gender(0)
+        @JvmField val FEMALE = Gender(1)
+    }
+
 }
---
+++ app/src/test/java/co/familytreeapp/model/GenderTest.kt
@@ -0,0 +1,30 @@
+package co.familytreeapp.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Test
+
+/**
+ * Unit tests for [Gender].
+ */
+class GenderTest {
+
+    @Test
+    fun genderTypeEqualityTest() {
+        val male = Gender(0)
+        val female = Gender(1)
+
+        assertEquals(male, Gender.MALE)
+        assertEquals(female, Gender.FEMALE)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun illegalValuesForGenderIdThrowException() {
+        val illegalGender1 = Gender(2)
+        fail("Expected an IllegalArgumentException to be thrown for $illegalGender1")
+
+        val illegalGender2 = Gender(-1)
+        fail("Expected an IllegalArgumentException to be thrown for $illegalGender2")
+    }
+
+}
