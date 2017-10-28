318c6f2cec8e27b616208deb62899e68425ce470
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-10-28 17:46:39 +0100

    Add unit test for TreeListItem

---
+++ app/src/test/kotlin/co/familytreeapp/model/TreeListItemTest.kt
@@ -0,0 +1,17 @@
+package co.familytreeapp.model
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Unit tests for [TreeListItem]
+ */
+class TreeListItemTest {
+
+    @Test(expected = IllegalArgumentException::class)
+    fun requireDepthAtLeastZero() {
+        TreeListItem("", -1)
+        Assert.fail("Expected an IllegalArgumentException to be thrown since depth < 0")
+    }
+
+}
