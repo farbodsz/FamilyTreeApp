c801a94d28041e7641c17be1c866292116b12a5d
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2018-01-24 10:27:58 +0000

    Add TreeListActivity separate from MainActivity

--- app/src/main/kotlin/co/familytreeapp/ui/MainActivity.kt
+++ app/src/main/kotlin/co/familytreeapp/ui/MainActivity.kt
@@ -1,8 +1,12 @@
-package co.familytreeapp
+package co.familytreeapp.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import co.familytreeapp.R
 
+/**
+ * The main screen of the app.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/kotlin/co/familytreeapp/ui/TreeListActivity.kt
@@ -0,0 +1,19 @@
+package co.familytreeapp.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import co.familytreeapp.R
+
+/**
+ * Activity for displaying the tree as a vertical list with indents.
+ */
+class TreeListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tree_list)
+
+        // TODO
+    }
+
+}
