553c9ac8491cb5e1f65f386c0a0b987e555d27aa
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2018-01-26 12:37:55 +0000

    Add navdrawer layout and helper functions for MainActivity

--- app/src/main/kotlin/co/familytreeapp/ui/MainActivity.kt
+++ app/src/main/kotlin/co/familytreeapp/ui/MainActivity.kt
@@ -1,17 +1,19 @@
 package co.familytreeapp.ui
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import co.familytreeapp.R
 
 /**
  * The main screen of the app.
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : NavigationDrawerActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(withNavigation(R.layout.activity_main))
     }
+
+    override fun getSelfNavigationParams() =
+            standardNavigationParams(NAVDRAWER_ITEM_MAIN, findViewById(R.id.toolbar))
 
 }
