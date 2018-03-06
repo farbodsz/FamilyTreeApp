9af4e2a795ab5c3bbfb04097f185a7a9f1e83dab
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2018-03-06 16:52:48 +0000

    Add home tile classes

---
+++ app/src/main/kotlin/com/farbodsz/familytree/ui/home/HomeTiles.kt
@@ -0,0 +1,36 @@
+package com.farbodsz.familytree.ui.home
+
+import android.content.Context
+import android.support.annotation.ColorInt
+import android.support.v4.content.ContextCompat
+import com.farbodsz.familytree.R
+import com.farbodsz.familytree.util.OnClick
+
+enum class HomeTiles {
+    VIEW_TREE,
+    PEOPLE
+}
+
+/**
+ * Holds data about a tile being displayed on the main page.
+ */
+sealed class HomeTile(
+        val title: String,
+        val description: String,
+        @ColorInt val color: Int,
+        val onClick: OnClick
+)
+
+class ViewTreeTile(context: Context, onClick: OnClick) : HomeTile(
+        context.getString(R.string.home_item_tree_title),
+        context.getString(R.string.home_item_tree_desc),
+        ContextCompat.getColor(context, R.color.homeCard_tree_bkgd),
+        onClick
+)
+
+class PeopleTile(context: Context, peopleCount: Int, onClick: OnClick) : HomeTile(
+        context.getString(R.string.home_item_people_title),
+        context.resources.getQuantityString(R.plurals.home_item_people_desc, peopleCount, peopleCount),
+        ContextCompat.getColor(context, R.color.homeCard_people_bkgd),
+        onClick
+)
