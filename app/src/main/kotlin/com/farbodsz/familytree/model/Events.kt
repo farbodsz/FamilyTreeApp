64fa4efabcdb2b52f4cd28f66c91e45eaeafcaad
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2018-03-10 17:04:53 +0000

    Make Event a sealed class

--- app/src/main/kotlin/com/farbodsz/familytree/model/Events.kt
+++ app/src/main/kotlin/com/farbodsz/familytree/model/Events.kt
@@ -1,16 +1,21 @@
 package com.farbodsz.familytree.model
 
+import com.farbodsz.familytree.util.DATE_FORMATTER_EVENT
 import org.threeten.bp.LocalDate
 import org.threeten.bp.Month
 
 /**
  * Represents an annual recurring event related to a person, such as a birthday or anniversary.
  *
- * @param dayOfMonth    day of the month when the event first took place (e.g. 25)
- * @param month         [Month] of the event when the event first took place (e.g. February)
- * @param location      name of the location where the event took place
+ * @property dayOfMonth day of the month when the event first took place (e.g. 25)
+ * @property month      [Month] of the event when the event first took place (e.g. February)
+ * @property location   name of the location where the event took place
  */
-abstract class Event(dayOfMonth: Int, month: Month, location: String)
+sealed class Event(val dayOfMonth: Int, val month: Month, val location: String) {
+
+    fun getDateText() = LocalDate.of(0, month, dayOfMonth).format(DATE_FORMATTER_EVENT)
+
+}
 
 class Birthday(
         val personId: Int,
