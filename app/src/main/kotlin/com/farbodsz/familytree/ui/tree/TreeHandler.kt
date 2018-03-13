0ee62f18a650035912ec0e6f1823f94aa9f2d22c
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2018-03-13 21:45:54 +0000

    Remove debugging dummy tree

--- app/src/main/kotlin/com/farbodsz/familytree/ui/tree/TreeHandler.kt
+++ app/src/main/kotlin/com/farbodsz/familytree/ui/tree/TreeHandler.kt
@@ -5,13 +5,11 @@ import android.util.Log
 import android.view.ViewGroup
 import com.farbodsz.familytree.database.manager.ChildrenManager
 import com.farbodsz.familytree.database.manager.PersonManager
-import com.farbodsz.familytree.model.Gender
 import com.farbodsz.familytree.model.Person
 import com.farbodsz.familytree.model.tree.TreeNode
 import com.farbodsz.familytree.ui.widget.PersonView
 import com.farbodsz.familytree.ui.widget.TreeView
 import com.farbodsz.familytree.util.OnDataClick
-import org.threeten.bp.LocalDate
 
 /**
  * Manages setting up tree data and displaying it in the UI.
@@ -29,7 +27,6 @@ class TreeHandler(
 
     companion object {
         private const val LOG_TAG = "TreeHandler"
-        private const val DISPLAY_DUMMY_TREE = false // for debugging purposes only
     }
 
     /**
@@ -116,6 +113,8 @@ class TreeHandler(
      * @param person    the person who's tree will be displayed. It can be null to indicate that a
      *                  full tree (consisting of all/most people from the db) should be displayed.
      *
+     * @return root node of the tree to display, or null if there is no tree to display
+     *
      * @see getFullTree
      * @see ChildrenManager.getTree
      */
@@ -129,10 +128,6 @@ class TreeHandler(
      * @return the root node of the tree, or null if there is no tree to display
      */
     private fun getFullTree(): TreeNode<Person>? {
-        if (DISPLAY_DUMMY_TREE) {
-            return getDummyTree()
-        }
-
         val allPeople = PersonManager(context).getAll()
         if (allPeople.isEmpty()) {
             return null
@@ -157,52 +152,6 @@ class TreeHandler(
         }
 
         return nodeWithGreatestHeight
-    }
-
-    private fun getDummyTree(): TreeNode<Person> {
-        val dummyDate = LocalDate.now()
-
-        val grandparent = Person(1, "Pedar", "Salamat-Zadeh", Gender.MALE, dummyDate, "", dummyDate, "")
-        val parent1 = Person(2, "Farzaneh", "Salamat-Zadeh", Gender.FEMALE, dummyDate, "", null, "")
-        val parent2 = Person(3, "Naser", "Salamat-Zadeh", Gender.MALE, dummyDate, "", null, "")
-        val parent3 = Person(4, "Farshad", "Salamat-Zadeh", Gender.MALE, dummyDate, "", null, "")
-        val parent4 = Person(5, "Forough", "Salamat-Zadeh", Gender.FEMALE, dummyDate, "", null, "")
-        val child1 = Person(6, "Aniseh", "Zeighami", Gender.FEMALE, dummyDate, "", null, "")
-        val child2 = Person(7, "Sana", "Zeighami", Gender.FEMALE, dummyDate, "", null, "")
-        val child3 = Person(8, "Ayeh", "Zeighami", Gender.FEMALE, dummyDate, "", dummyDate, "")
-        val child4 = Person(9, "Ghazal", "Zeighami", Gender.FEMALE, dummyDate, "", null, "")
-        val child5 = Person(10, "Raouf", "Salamat-Zadeh", Gender.MALE, dummyDate, "", null, "")
-        val child6 = Person(11, "Khaled", "Salamat-Zadeh", Gender.MALE, dummyDate, "", null, "")
-        val child7 = Person(12, "Farbod", "Salamat-Zadeh", Gender.MALE, dummyDate, "", null, "")
-        val child8 = Person(13, "Fardis", "Salamat-Zadeh", Gender.FEMALE, dummyDate, "", null, "")
-        val child9 = Person(14, "Asal", "Pourmashal", Gender.FEMALE, dummyDate, "", null, "")
-        val child10 = Person(15, "Mehdi", "Pourmashal", Gender.MALE, dummyDate, "", null, "")
-        val grandchild1 = Person(16, "Fatemeh", "???", Gender.FEMALE, dummyDate, "", null, "")
-        val grandchild2 = Person(17, "Mohammad-Houssain", "???", Gender.MALE, dummyDate, "", null, "")
-
-        return TreeNode(grandparent).apply { addChildren(listOf(
-                TreeNode(parent1).apply { addChildren(listOf(
-                        TreeNode(child1),
-                        TreeNode(child2),
-                        TreeNode(child3)
-                )) },
-                TreeNode(parent2).apply { addChildren(listOf(
-                        TreeNode(child4).apply { addChildren(listOf(
-                                TreeNode(grandchild1),
-                                TreeNode(grandchild2)
-                        )) },
-                        TreeNode(child5),
-                        TreeNode(child6)
-                )) },
-                TreeNode(parent3).apply { addChildren(listOf(
-                        TreeNode(child7),
-                        TreeNode(child8)
-                )) },
-                TreeNode(parent4).apply { addChildren(listOf(
-                        TreeNode(child9),
-                        TreeNode(child10)
-                )) }
-        )) }
     }
 
 }
