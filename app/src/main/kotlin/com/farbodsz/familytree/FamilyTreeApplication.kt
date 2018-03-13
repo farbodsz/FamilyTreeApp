4222e585834b7162f5bb8c8837b6b285c74adea4
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2018-03-13 13:29:43 +0000

    Add brief docs for FamilyTreeApplication

--- app/src/main/kotlin/com/farbodsz/familytree/FamilyTreeApplication.kt
+++ app/src/main/kotlin/com/farbodsz/familytree/FamilyTreeApplication.kt
@@ -3,11 +3,15 @@ package com.farbodsz.familytree
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
 
+/**
+ * Application class for maintaining global application state.
+ */
 class FamilyTreeApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        // Initialise DateTime library
         AndroidThreeTen.init(this)
     }
 
